3f76fd0471a38f80ad34ca6e6f3f2725a05bd11a
Cory Spitzer <coryspitzer@gmail.com>
2016-02-18 09:58:51 -0500

    Create a new ruby project

--- new_ruby_project.sh
+++ new_ruby_project.sh
@@ -1,1 +1,8 @@
-echo "Creating files... "
+#!/bin/bash
+echo "Creating Ruby project named $1... "
+
+mkdir $1
+cd $1
+touch $1.rb $1_spec.rb
+git init
+git remote add origin git@github.com:CorySpitzer/$1.git
