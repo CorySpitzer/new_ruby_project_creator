7a92c02d19ff32e03142c62dbbf5d989ceaba6df
Cory Spitzer <coryspitzer@gmail.com>
2016-02-20 09:22:15 -0500

    Move new_repo.sh to it's own project

--- new_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Creating a new repository on Github... "
-
-# Create a new Github repo using their API:
-curl -u 'coryspitzer' https://api.github.com/user/repos -d '{"name":"'$1'"}'
